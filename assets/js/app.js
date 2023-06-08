fb76101d4ce3580c54c64dcb1454351f8e656d57
Corentin-Devienne <117612198+Corentin-Devienne@users.noreply.github.com>
2023-06-08 11:44:46 +0200

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 const dialogs=[
     {question:"toto",answer:"tata"}, 
     {question:"Salut", answer:"Salut"}, 
-    {question:"ça va ?", answer:"bien et toi ?"}
+    {question:"ça va ?", answer:"bien et toi ?"},
+    {question:"comment tu t'apelle ?", answer:"je m'appelle Bob"},
+    {question:"Quelle temps fait il a Boulogne?", answer:"Pas d'API alors je peux pas te dire en temps réel mais il fait surrement froid comme d'hab :/"},
 ]
 
 const datalist = document.querySelector("#datalist");
